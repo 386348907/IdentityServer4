65a36f9ecbda59a9f8376ace40ae112936d3fa5f
ghp <386348907@qq.com>
2020-01-25 19:28:26 +0800

    添加项目文件。

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    //F3 创建配置类   
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        /// <summary>
+        /// 返回APIResource
+        /// </summary>
+        public static IEnumerable<ApiResource> GetApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("Api1", "My Api1")
+            };
+
+
+        /// <summary>
+        /// 创建客户端
+        /// </summary>
+        public static IEnumerable<Client> GetClient()
+        {
+
+
+            return new List<Client> {
+
+
+                new Client()
+                 {
+                ClientId = "client1",
+                
+                //配置验证类型为客户端验证
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                //加密Secret   
+               ClientSecrets =new List<Secret> {
+                       new Secret("Secret1".Sha256())
+                   },
+
+               //该客户端可以访问的Resource
+                AllowedScopes = { "Api1" }
+                  } 
+            
+            };
+
+        }
+    }
+
+
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            //services.AddControllersWithViews();
+
+            //F1 引用IdentityServer 服务 
+            //AddIdentityServer 会将 IdentityServer 注册到 DI
+            //。他还会注册一个基于内存存储的运行时状态，
+            //这对于开发场景来说是很有用的。对于生产环境你就需要像数据库或缓存这些持久化或共享存储部件。
+            services.AddIdentityServer().AddDeveloperSigningCredential()
+               
+                
+                ///////F4  注入APi和Client
+                //注入ApiResources   根据我们的config类进行创建APi资源
+                .AddInMemoryApiResources(Config.GetApiResources)
+                //注入客户端    根据config类配置的信息进行创建客户端   使用时请求需要指定客户端进行对应的授权   不同的客户端可以配置不同的权限
+                .AddInMemoryClients(Config.GetClient());
+       
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+       //F2 插入中间件
+            app.UseIdentityServer();
+
+            
+
+
+             
+        }
+    }
+}
